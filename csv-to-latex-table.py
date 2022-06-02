740953855165034a6ef372d26489ed0c82193a5b
zoernchen <105123263+zoernchen@users.noreply.github.com>
2022-06-02 14:20:29 +0200

    Update and rename 05_csv_to_latex_table.py to csv-to-latex-table.py

--- csv-to-latex-table.py
+++ csv-to-latex-table.py
@@ -1,22 +1,23 @@
 # csv file to LaTeX table
+# you can copy the printed table and insert it in your LaTeX document
 
 import numpy as np
 import pandas as pd
-import math
-from decimal import *
-import csv
 
+#import data
 name= "A04_ergebnisse"
 path = f"K:/work/Physikpraktikum/A02/{name}_rounded"
 df = pd.read_csv(f"{path}.csv", header=None) #set header to None if data starts in first line of document
 data = df.to_numpy() #convert pandas data frame to numpy array
 
+#data
 x = data[:,0]
 y1 = data[:,1]
-
+#errors
 xerr = data[:,2]
 y1err = data[:,3]
 
+#print data and errors in right order with LaTeX-ready syntax
 for i in range(0, len(x)):
     s = f"{x[i]} & {y1[i]} & {xerr[i]} & {y1err[i]} //"
     print(s)
